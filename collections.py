db75e93a67670d2febdf7279c149649d7678969a
EC2 Default User <ec2-user@ip-10-16-10-253.us-west-2.compute.internal>
2024-10-15 13:21:32 +0000

    Lab 113 composite Tpye

--- collections.py
+++ collections.py
@@ -1,15 +1,4 @@
-import csv
-import copy
+
 myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
 for item in myMixedTypeList:
     print("{} is of the data type {}".format(item,type(item)))
-myVehicle = {
-    "vin" : "<empty>",
-    "make" : "<empty>" ,
-    "model" : "<empty>" ,
-    "year" : 0,
-    "range" : 0,
-    "topSpeed" : 0,
-    "zeroSixty" : 0.0,
-    "mileage" : 0
-}
